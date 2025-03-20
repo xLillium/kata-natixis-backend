d4ce5dbf91d73ea0b07baa6cc40acfb60b03c55c
xLillium <nicolas.motillon@gmail.com>
2025-03-20 17:02:35 +0100

    test: add integration test for GET /api/books

---
+++ src/test/java/com/xlillium/kata_natixis_backend/controllers/BookControllerTest.java
@@ -0,0 +1,30 @@
+package com.xlillium.kata_natixis_backend.controllers;
+
+import com.xlillium.kata_natixis_backend.BaseIntegrationTest;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@AutoConfigureMockMvc
+public class BookControllerTest extends BaseIntegrationTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testGetBooks() throws Exception {
+        mockMvc.perform(get("/api/books"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").isArray())
+                .andExpect(jsonPath("$[0].id").value("1"))
+                .andExpect(jsonPath("$[0].title").value("1984"))
+                .andExpect(jsonPath("$[0].author").value("George Orwell"))
+                .andExpect(jsonPath("$[0].isbn").value("9780155658110"))
+                .andExpect(jsonPath("$[0].borrowed").value("true"));
+    }
+}
