2f70600d13e29d8a6ba30eb2f3d579e11a0307cd
xLillium <nicolas.motillon@gmail.com>
2025-03-20 16:16:06 +0100

    Init project

---
+++ src/main/java/com/xlillium/kata_natixis_backend/KataNatixisBackendApplication.java
@@ -0,0 +1,13 @@
+package com.xlillium.kata_natixis_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KataNatixisBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KataNatixisBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xlillium/kata_natixis_backend/KataNatixisBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xlillium.kata_natixis_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KataNatixisBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
