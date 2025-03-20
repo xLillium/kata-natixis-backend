8596dabad590ee50f9cc08ed51edc91d82207973
xLillium <nicolas.motillon@gmail.com>
2025-03-20 17:02:27 +0100

    test: add BaseIntegrationTest for consistent test data

--- src/test/java/com/xlillium/kata_natixis_backend/BaseIntegrationTest.java
+++ src/test/java/com/xlillium/kata_natixis_backend/BaseIntegrationTest.java
@@ -1,9 +1,21 @@
-package com.xlillium.kata_natixis;
+package com.xlillium.kata_natixis_backend;
 
+import com.xlillium.kata_natixis_backend.models.Book;
+import com.xlillium.kata_natixis_backend.repositories.BookRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
 public abstract class BaseIntegrationTest {
+    @Autowired
+    private BookRepository bookRepository;
+    @BeforeEach
+    void setUp() {
+        bookRepository.deleteAll();
+        bookRepository.save(new Book("1984", "George Orwell", "9780155658110", true));
+        bookRepository.save(new Book("Clean Code", "Robert C. Martin", "9780132350884", false));
+    }
 }
