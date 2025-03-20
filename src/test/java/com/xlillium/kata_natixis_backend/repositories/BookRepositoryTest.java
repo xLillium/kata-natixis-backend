a9e1919f6c878e5c95bcf085f12c095884a48c18
xLillium <nicolas.motillon@gmail.com>
2025-03-20 17:02:44 +0100

    test: add repository test for BookRepository

--- src/test/java/com/xlillium/kata_natixis_backend/repositories/BookRepositoryTest.java
+++ src/test/java/com/xlillium/kata_natixis_backend/repositories/BookRepositoryTest.java
@@ -1,2 +1,24 @@
-package com.xlillium.kata_natixis_backend.repositories;public class BookRepositoryTest {
+package com.xlillium.kata_natixis_backend.repositories;
+
+import com.xlillium.kata_natixis_backend.BaseIntegrationTest;
+import com.xlillium.kata_natixis_backend.models.Book;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class BookRepositoryTest extends BaseIntegrationTest {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    @Test
+    void testFindAll() {
+        List<Book> books = bookRepository.findAll();
+        assertThat(books).hasSize(2);
+    }
 }
