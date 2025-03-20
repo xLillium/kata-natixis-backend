fa2c5736d2b5e21dc019d79631fd4c7bd5118eec
xLillium <nicolas.motillon@gmail.com>
2025-03-20 16:42:13 +0100

    feat: configure H2 db and add Book entity and add initial data

---
+++ src/main/java/com/xlillium/kata_natixis_backend/models/Book.java
@@ -0,0 +1,66 @@
+package com.xlillium.kata_natixis_backend.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "books")
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String author;
+    @Column(unique = true)
+    private String isbn;
+    private boolean borrowed;
+
+    public Book() {
+    }
+
+    public Book(String title, String author, String isbn, boolean borrowed) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+        this.borrowed = borrowed;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public boolean isBorrowed() {
+        return borrowed;
+    }
+
+    public void setBorrowed(boolean borrowed) {
+        this.borrowed = borrowed;
+    }
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO books (title, author, isbn, borrowed) VALUES ('1984', 'George Orwell', '9780155658110', true);
+INSERT INTO books (title, author, isbn, borrowed) VALUES ('Clean Code', 'Robert C. Martin', '9780132350884', false);
+INSERT INTO books (title, author, isbn, borrowed) VALUES ('Software craft: TDD, Clean Code et autres pratiques essentielles', 'Cyrille Martraire', '2100825208', false);
+INSERT INTO books (title, author, isbn, borrowed) VALUES ('Implementing Domain-Driven Design', 'Vaughn Vernon', '9780321834577', false);
