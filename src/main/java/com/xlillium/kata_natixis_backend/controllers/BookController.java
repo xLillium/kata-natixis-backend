4cdf47a008620ea53e105ae820a252b45bb3c2de
xLillium <nicolas.motillon@gmail.com>
2025-03-20 17:01:26 +0100

    feat: add GET /api/books endpoint

--- src/main/java/com/xlillium/kata_natixis_backend/controllers/BookController.java
+++ src/main/java/com/xlillium/kata_natixis_backend/controllers/BookController.java
@@ -1,2 +1,24 @@
-package com.xlillium.kata_natixis_backend.controllers;public class BookController {
+package com.xlillium.kata_natixis_backend.controllers;
+
+import com.xlillium.kata_natixis_backend.models.Book;
+import com.xlillium.kata_natixis_backend.repositories.BookRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/books")
+public class BookController {
+    private final BookRepository bookRepository;
+
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @GetMapping
+    List<Book> getBooks() {
+        return bookRepository.findAll();
+    }
 }
